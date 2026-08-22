186aaf2f4cbbdc445d5a6bd29a59dee40018e486
Adler José <adlerjose.work@gmail.com>
2026-08-22 01:10:33 -0300

    feat: adiciona menu do gerenciador de tarefas

---
+++ manager.py
@@ -0,0 +1,15 @@
+while True:
+    print("\nGerenciador de Tarefas:")
+    print("1. Adicionar tarefa")
+    print("2. Visualizar tarefa")
+    print("3. Atualizar tarefa")
+    print("4. Completar tarefa")
+    print("5. Deletar tarefa")
+    print("6. Sair")
+
+    choice = int(input("Digite uma opção: "))
+
+    if choice == 6:
+        break
+
+print("Programa finalizado com sucesso!")
